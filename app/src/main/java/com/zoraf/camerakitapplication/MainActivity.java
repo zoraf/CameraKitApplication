644d3b2ee299caf98adcf8d4f357ac82612c58a0
zoraf <mzoraf09@gmail.com>
2020-09-21 16:01:07 +0600

    camera kit is implemented but it is showing some error

--- app/src/main/java/com/zoraf/camerakitapplication/MainActivity.java
+++ app/src/main/java/com/zoraf/camerakitapplication/MainActivity.java
@@ -1,175 +1,243 @@
 package com.zoraf.camerakitapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import com.wonderkiln.camerakit.CameraKit;
-import com.wonderkiln.camerakit.CameraKitError;
-import com.wonderkiln.camerakit.CameraKitEvent;
-import com.wonderkiln.camerakit.CameraKitEventListener;
-import com.wonderkiln.camerakit.CameraKitImage;
-import com.wonderkiln.camerakit.CameraKitVideo;
-import com.wonderkiln.camerakit.CameraView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.camerakit.CameraKit;
+import com.camerakit.CameraKitView;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
-    public static final String BROADCAST_PC_TAKEN_ACTION = "com.unipos.tc.picktiretaken";
-
-    private CameraView cameraView;
-    private Button captureBtn;
-    public static Bitmap cubitmap;
-    public static String unix_time_stamp_name;
-    public static String subPath;
-    public static String fileName;
-
-    public static String STATIC_ID, BOL_S, arrive_s;
-    public static int STATIC_NEWCODATARECID;
-    public static String Type;
-    public String root;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = "MainActivity";
+    private CameraKitView mCameraKitView;
+    private Button mCaptureImageButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cameraView = (CameraView) findViewById(R.id.cameraView);
-        captureBtn = (Button) findViewById(R.id.captureBtn);
+        mCameraKitView = findViewById(R.id.camera);
+        initCameraKitView();
+        mCaptureImageButton = findViewById(R.id.btnCapture);
+        mCaptureImageButton.setOnClickListener(this);
+    }
 
-        cameraView.setJpegQuality(50);
-        cameraView.setFocus(CameraKit.Constants.FOCUS_TAP);
-        cameraView.setFlash(CameraKit.Constants.FLASH_AUTO);
-        cameraView.setMethod(CameraKit.Constants.METHOD_STILL);
-
-        cameraView.addCameraKitListener(new CameraKitEventListener() {
-            @Override
-            public void onEvent(CameraKitEvent cameraKitEvent) {
-
-            }
-
-            @Override
-            public void onError(CameraKitError cameraKitError) {
-
-            }
-
-            @Override
-            public void onImage(CameraKitImage cameraKitImage) {
-
-                Bitmap bmap = cameraKitImage.getBitmap();
-
-                cubitmap = bmap;
-                saveImage(bmap);
-            }
-
-            @Override
-            public void onVideo(CameraKitVideo cameraKitVideo) {
-
-            }
-        });
-
-        captureBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                cameraView.captureImage();
-            }
-        });
-
+    private void initCameraKitView() {
+        mCameraKitView.setFacing(CameraKit.FACING_BACK);
+        mCameraKitView.setFocus(CameraKit.FOCUS_AUTO);
+        mCameraKitView.setZoomFactor(1.0f);
 
     }
 
-
-    private BroadcastReceiver imgConfBroadcastReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            saveImage(cubitmap);
-        }
-    };
-
-
     @Override
-    protected void onResume() {
-        super.onResume();
-        cameraView.start();
+    protected void onStart() {
+        super.onStart();
+        mCameraKitView.onStart();
     }
 
     @Override
     protected void onPause() {
-        cameraView.stop();
+        mCameraKitView.onPause();
         super.onPause();
+
     }
 
     @Override
-    protected void onDestroy() {
-
-        try {
-            unregisterReceiver(imgConfBroadcastReceiver);
-        } catch (IllegalArgumentException e) {
-
-        }
-
-        super.onDestroy();
+    protected void onResume() {
+        super.onResume();
+        mCameraKitView.onResume();
     }
 
-    public void saveImage(Bitmap bitmap) {
-        unix_time_stamp_name = "";
-        BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+    @Override
+    protected void onStop() {
+        mCameraKitView.onStop();
+        super.onStop();
 
+    }
 
-        String path = root + "/NTASKER/";
-        FileOutputStream outFile = null;
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        mCameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 
-        File file = new File(path, unix_time_stamp_name);
-
-        try {
-
-            outFile = new FileOutputStream(file);
-
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outFile);
-
-            outFile.flush();
-
-            outFile.close();
-
-            fileName = file.getPath();
-
-            if(CameraKitActivity.Type.equals("EditTask")) {
-                Intent intent = new Intent(BROADCAST_PC_TAKEN_ACTION);
-                sendBroadcast(intent);
-            }
-
-            try {
-
-            } catch (Exception e) {
-                saveImagetoDatabse();
-            }
-
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnCapture:
+                mCameraKitView.captureImage(new CameraKitView.ImageCallback() {
+                    @Override
+                    public void onImage(CameraKitView cameraKitView, byte[] capturedImage) {
+                        Log.d(TAG, "onImage: " + Environment.getExternalStorageDirectory());
+                        File savedPhoto = new File(Environment.getExternalStorageDirectory(), "photo.jpg");
+                        try {
+                            FileOutputStream outputStream = new FileOutputStream(savedPhoto.getPath());
+                            outputStream.write(capturedImage);
+                            outputStream.close();
+                        } catch (java.io.IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                break;
         }
-        catch (FileNotFoundException e) {
-
-            e.printStackTrace();
-
-        } catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-
-        this.finish();
     }
 }
+
+//public class MainActivity extends AppCompatActivity {
+//    public static final String BROADCAST_PC_TAKEN_ACTION = "com.unipos.tc.picktiretaken";
+//
+//    private CameraKitView cameraKitView;
+//    private CameraView cameraView;
+//    private Button captureBtn;
+//    public static Bitmap cubitmap;
+//    public static String unix_time_stamp_name;
+//    public static String subPath;
+//    public static String fileName;
+//
+//    public static String STATIC_ID, BOL_S, arrive_s;
+//    public static int STATIC_NEWCODATARECID;
+//    public static String Type;
+//    public String root;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        cameraView = (CameraView) findViewById(R.id.cameraView);
+//        captureBtn = (Button) findViewById(R.id.captureBtn);
+//
+//        cameraView.setJpegQuality(50);
+//        cameraView.setFocus(CameraKit.Constants.FOCUS_TAP);
+//        cameraView.setFlash(CameraKit.Constants.FLASH_AUTO);
+//        cameraView.setMethod(CameraKit.Constants.METHOD_STILL);
+//
+//        cameraView.addCameraKitListener(new CameraKitEventListener() {
+//            @Override
+//            public void onEvent(CameraKitEvent cameraKitEvent) {
+//
+//            }
+//
+//            @Override
+//            public void onError(CameraKitError cameraKitError) {
+//
+//            }
+//
+//            @Override
+//            public void onImage(CameraKitImage cameraKitImage) {
+//
+//                Bitmap bmap = cameraKitImage.getBitmap();
+//
+//                cubitmap = bmap;
+//                saveImage(bmap);
+//            }
+//
+//            @Override
+//            public void onVideo(CameraKitVideo cameraKitVideo) {
+//
+//            }
+//        });
+//
+//        captureBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                cameraView.captureImage();
+//            }
+//        });
+//
+//
+//    }
+//
+//
+//    private BroadcastReceiver imgConfBroadcastReceiver = new BroadcastReceiver() {
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            saveImage(cubitmap);
+//        }
+//    };
+//
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        cameraView.start();
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        cameraView.stop();
+//        super.onPause();
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//
+//        try {
+//            unregisterReceiver(imgConfBroadcastReceiver);
+//        } catch (IllegalArgumentException e) {
+//
+//        }
+//
+//        super.onDestroy();
+//    }
+//
+//    public void saveImage(Bitmap bitmap) {
+//        unix_time_stamp_name = "";
+//        BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+//
+//
+//        String path = root + "/NTASKER/";
+//        FileOutputStream outFile = null;
+//
+//        File file = new File(path, unix_time_stamp_name);
+//
+//        try {
+//
+//            outFile = new FileOutputStream(file);
+//
+//            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outFile);
+//
+//            outFile.flush();
+//
+//            outFile.close();
+//
+//            fileName = file.getPath();
+//
+//            if(CameraKitActivity.Type.equals("EditTask")) {
+//                Intent intent = new Intent(BROADCAST_PC_TAKEN_ACTION);
+//                sendBroadcast(intent);
+//            }
+//
+//            try {
+//
+//            } catch (Exception e) {
+//                saveImagetoDatabse();
+//            }
+//
+//        }
+//        catch (FileNotFoundException e) {
+//
+//            e.printStackTrace();
+//
+//        } catch (IOException e)
+//        {
+//            e.printStackTrace();
+//        }
+//        catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+//
+//        this.finish();
+//    }
+//}
